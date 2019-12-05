dd405cdf196db80a907d8e9510871e0fed56c75b
Casper Smet <casper.smet@student.hu.nl>
2019-12-05 14:40:20 +0100

    Added Time class

--- route_66/model.py
+++ route_66/model.py
@@ -1,4 +1,5 @@
 from mesa.space import SingleGrid
+from mesa.time import StagedActivation
 from mesa import Model
 
 class RoadModel(Model):
@@ -6,7 +7,7 @@ class RoadModel(Model):
     def __init__(self, N, length, lanes=1):
         self.num_agents = N 
         self.grid = SingleGrid(length, lanes, True)
-        # Time tbd
+        self.schedule = StagedActivation(self)
 
     def step(self):
         pass
