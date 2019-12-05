b2192fe1907a1a94233cdcbc3e709916a7e0e34c
Casper Smet <casper.smet@student.hu.nl>
2019-12-05 15:56:27 +0100

    Added the velocities of one agent over time to a plot

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 from route_66.model import RoadModel
+import matplotlib.pyplot as plt
 
 model = RoadModel(6, 60)
 for _ in range(100):
     model.step()
 
-    
+velocities = model.datacollector.get_agent_vars_dataframe()
+
+agent0 = velocities.reset_index()
+
+agent0 = agent0[agent0["AgentID"] == 0]
+agent0.set_index("Step", inplace=True)
+print(agent0.head())
+
+agent0[["Velocity"]].plot()
+
+
+plt.show()
