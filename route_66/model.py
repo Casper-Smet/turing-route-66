4c76916347b04c8a852d45a84aacaced1a32a588
Casper Smet <casper.smet@student.hu.nl>
2019-12-05 14:38:01 +0100

    Added space class

--- route_66/model.py
+++ route_66/model.py
@@ -1,11 +1,11 @@
-
+from mesa.space import SingleGrid
 from mesa import Model
 
 class RoadModel(Model):
     """A model with a number of cars, Nagel-Schreckenberg"""
     def __init__(self, N, length, lanes=1):
         self.num_agents = N 
-        # GRID tbd
+        self.grid = SingleGrid(length, lanes, True)
         # Time tbd
 
     def step(self):
