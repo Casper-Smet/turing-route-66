3c71f5ea2202c53a888b16f247eaf7b7359c2f57
denbergvanthijs <denbergvanthijs@gmail.com>
2019-12-06 14:49:38 +0100

    Improved visualisation with better marker sizes and optional grid

--- route_66/visualisation.py
+++ route_66/visualisation.py
@@ -2,25 +2,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from route_66.model import RoadModel
+from route_66.agent import CarAgent
 
 
-def plot(steps, N, length=60, lanes=1):
+def plot(steps, N, length=100, lanes=1, p=0.5, grid=False):
+    """Visualises the development of traffic throughput"""
+    assert N < length, "It is not possible to have more cars than cells"
     model = RoadModel(N, length=length, lanes=lanes)
+    CarAgent.p = p
+
     for _ in range(steps):
         model.step()
 
     velocities = model.datacollector.get_agent_vars_dataframe()
     velocities.reset_index(inplace=True)
     velocities["Position"] = velocities['Position'].apply(lambda x: x[0] + 0.5)
+    velocities["Step"] = velocities["Step"].apply(lambda x: x + 0.5)
 
-    fig, ax = plt.subplots()
-    ax.scatter(velocities["Position"], velocities["Step"], marker="s", color="black")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    ax.scatter(velocities["Position"], velocities["Step"], marker="s", color="black", s=20, zorder=1)
 
-    # ax.invert_yaxis()
+    if grid:
+        spacing = np.arange(0, steps)
+        plt.hlines(spacing, length, 0, color="grey", alpha=0.3, zorder=0)
+        plt.vlines(spacing, steps, 0, color="grey", alpha=0.3, zorder=0)
 
+    plt.title(f"Simulation of {N} cars during {steps} time steps \non a continuous road of {length} cells long.")
     ax.set_xlabel("Position on the road")
     ax.set_ylabel(f"Time: 0 to {steps}")
     ax.set_xlim(0, length)
     ax.set_ylim(0, steps)
 
+    ax.set_aspect('equal')
+    plt.gca().invert_yaxis()
     plt.show()
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from route_66.server import server
 from route_66.visualisation import plot
 
 # server.launch()
-plot(100, 6)
+plot(100, 20, length=100, p=0.3, grid=True)
