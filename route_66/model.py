f03c6c975def1f92ed91c8468e43e2738303be5d
Casper Smet <casper.smet@student.hu.nl>
2019-12-06 13:13:28 +0100

    Model now collects average velocity of all agents

--- route_66/model.py
+++ route_66/model.py
@@ -4,6 +4,7 @@ from mesa import Model
 from mesa.space import SingleGrid
 from mesa.time import StagedActivation
 from mesa.datacollection import DataCollector
+import numpy as np
 
 
 class RoadModel(Model):
@@ -23,12 +24,19 @@ class RoadModel(Model):
             # Add to grid (randomly)
             self.grid.position_agent(a)
 
+        self.average_velocity = CarAgent.init_velocity
         self.datacollector = DataCollector(agent_reporters={
             "Position": "pos",
-            "Velocity": "velocity"})
+            "Velocity": "velocity"}, 
+            model_reporters={
+            "Average Velocity" : "average_velocity"})
 
         self.running = True
 
     def step(self):
+        # Calculate average velocity
+        self.average_velocity = np.mean([a.velocity for a in self.schedule.agents])
+        # Collect data
         self.datacollector.collect(self)
+        # Run next step
         self.schedule.step()
