fc88b8dfc73ddf8f20dfb7656761c0fa7dfa26d3
denbergvanthijs <denbergvanthijs@gmail.com>
2019-12-10 15:25:01 +0100

    Improved code for batch runner, more arguments

--- route_66/batch_run.py
+++ route_66/batch_run.py
@@ -1,19 +1,20 @@
 from mesa.batchrunner import BatchRunner
 from route_66.model import RoadModel, get_average_velocity
+from route_66.agent import CarAgent
 import matplotlib.pyplot as plt
 import numpy as np
 
 # https://github.com/Casper-Smet/intro-to-agents/blob/master/batch_run.py
 
 
-def run_batch():
+def run_batch(N=[35], timer=[2]):
     fixed_params = {
         "length": 100,
         "lanes": 1
     }
 
-    variable_params = {"N": [35, 50, 80],
-                       "timer": range(0, 10)}
+    variable_params = {"N": N,
+                       "timer": timer}
 
     # The variables parameters will be invoke along with the fixed parameters allowing for either or both to be honored.
     batch_run = BatchRunner(
@@ -26,20 +27,18 @@ def run_batch():
             "Average Velocity": get_average_velocity})
 
     batch_run.run_all()
-
     run_data = batch_run.get_model_vars_dataframe()
-    print(run_data.tail())
     run_data_gb = run_data.groupby("N")
+
     groups = [run_data_gb.get_group(x) for x in run_data_gb.groups]
-
-    fig, ax = plt.subplots()
-
-    ns = [35, 50, 80]
-    for group, n in zip(groups, ns):
-        ax.scatter(np.arange(0, 50), group["Average Velocity"], label=n)
+    for group, n in zip(groups, N):
+        plt.scatter(np.arange(0, group.shape[0]), group["Average Velocity"], label=n)
 
     plt.legend()
     plt.xlabel("Run")
     plt.ylabel("Average Velocity")
+    plt.xlim(0, group.shape[0])
+    plt.ylim(0, CarAgent.max_velocity)
 
+    print(run_data.head())
     plt.show()
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from route_66.batch_run import run_batch
 
 # server.launch()
 # plot(100, 35, length=100, p=0.3, grid=True)
-run_batch()
+run_batch(N=[10, 25, 30], timer=[2, 3, 4, 5, 6, 7, 8])
