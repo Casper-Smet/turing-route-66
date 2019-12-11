915605636aaa60327dfcd15f9c9517700caa4d2f
Casper Smet <casper.smet@student.hu.nl>
2019-12-11 09:49:40 +0100

    Removed plotting function calls from run.py

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
 from route_66.server import server
-from route_66.visualisation import plot
-from route_66.batch_run import plot_batch
 
-# server.launch()
-# plot(100, 35, length=100, p=0.3, grid=True)
-plot_batch(N=[10, 35, 70], timer=[0, 2, 6])
+server.launch()
