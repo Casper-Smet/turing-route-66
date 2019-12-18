1f9ceb5b5a3fd24271a1bff8679c692288810e4c
denbergvanthijs <denbergvanthijs@gmail.com>
2019-12-18 12:05:21 +0100

    Plot timing deleted, plot batch run refactored

--- route_66/batch_run.py
+++ route_66/batch_run.py
@@ -7,7 +7,7 @@ from route_66.model import RoadModel, get_average_velocity, get_standard_deviati
 from route_66.model import get_on_ramp_queue, get_waiting_queue
 
 
-def run_batch(N=[35], timer=[2], iterations=5):
+def run_batch(N=[35], timer=[2], iterations=5, max_steps=100):
     """
     Runs the simulation for every combination of N and timer
     N is the number of Cars on the road
@@ -16,6 +16,7 @@ def run_batch(N=[35], timer=[2], iterations=5):
     :param N: List of each number of initial cars on the road
     :param timer: List of each timing that needs to be simulated
     :param iterations: Integer of the amount of times every single combination needs to be repeated
+    :param max_steps: Maximum amount of steps each simulation will run
 
     :return: DataFrame with the Average Velocity and Standard Deviation of the velocity for every combination
     """
@@ -34,7 +35,7 @@ def run_batch(N=[35], timer=[2], iterations=5):
         variable_params,
         fixed_params,
         iterations=iterations,  # Iterations per combination of parameters
-        max_steps=100,
+        max_steps=max_steps,
         model_reporters={
             "Average Velocity": get_average_velocity,   # Average velocity per simulation
             "Standard Deviation": get_standard_deviation_velocity,
@@ -43,36 +44,4 @@ def run_batch(N=[35], timer=[2], iterations=5):
 
     batch_run.run_all()  # Run all simulations
     run_data = batch_run.get_model_vars_dataframe()  # Get DataFrame with collected data
-    return run_data
-
-
-def plot_batch(N=[35], timer=[2], iterations=5):
-    """
-    Plots each combination of N and timer.
-    Each combination has its own coloured dot.
-    Each combination is run for several iterations, thus several dots.
-
-    :param N: List of each number of initial cars on the road
-    :param timer: List of each timing that needs to be simulated
-    :param iterations: Integer of the amount of times every single combination needs to be repeated
-
-    :return: the DataFrame associated with the batch plot
-    """
-    run_data = run_batch(N=N, timer=timer, iterations=iterations)
-    run_data["Run"] += 1  # Run starts now at 1, better for plot limits
-
-    _, ax = plt.subplots(figsize=(12, 8))
-    # ax = sns.scatterplot("Run", "Average Velocity", data=run_data, size="N", hue="timer", palette="Set1")
-    sns.boxplot(x="timer", y="Average Velocity", hue="N", data=run_data, ax=ax)
-
-    ax.set_title(f"{run_data.shape[0]} runs of the simulation using multiple combinations of 'N' and 'timer'")
-    ax.set_xlabel(f"Different timers: {timer}")
-    ax.set_ylabel("Average Velocity")
-    ax.set_xlim(-0.5, len(timer) - 0.5)
-    ax.set_ylim(0, CarAgent.max_velocity)
-
-    plt.legend()
-    plt.show()
-
-    # Returns the same data as used in the plot, run_batch() could be used if the data does not need to be the same as plot
     return run_data
--- route_66/visualisation.py
+++ route_66/visualisation.py
@@ -31,8 +31,9 @@ def plot_simulation(steps, N, length=100, lanes=1, p=0.5, grid=False):
     velocities["Position"] = velocities['Position'].apply(lambda x: x[0] + 0.5)
     velocities["Step"] = velocities["Step"].apply(lambda x: x + 0.5)
 
+    sns.set("notebook")
     _, ax = plt.subplots(figsize=(8, 8))
-    ax.scatter(velocities["Position"], velocities["Step"], marker="s", color="black", s=10, zorder=1)
+    sns.scatterplot(velocities["Position"], velocities["Step"], marker="s", color="black", s=10, zorder=1, ax=ax)
 
     if grid:
         spacing = np.arange(0, steps)
@@ -50,10 +51,33 @@ def plot_simulation(steps, N, length=100, lanes=1, p=0.5, grid=False):
     plt.show()
 
 
-def plot_queues(N=[10, 25, 35], timer=[0, 1, 2, 4, 6], iterations=20):
-    run_data = run_batch(N=N, timer=timer, iterations=iterations)
+def plot_batch_run(N=[35], timer=[2], iterations=5, max_steps=100):
+    """
+    Plots each combination of N and timer.
+    Each combination has its own coloured dot.
+    Each combination is run for several iterations, thus several dots.
 
-    sns.set("paper")
-    _, ax = plt.subplots()
-    sns.scatterplot("timer", "Waiting Queue", data=run_data, label="Waiting Queue, 2 cars per step", ax=ax)
-    sns.scatterplot("timer", "On Ramp Queue", hue="N", data=run_data, ax=ax)
+    :param N: List of each number of initial cars on the road
+    :param timer: List of each timing that needs to be simulated
+    :param iterations: Integer of the amount of times every single combination needs to be repeated
+
+    :return: the DataFrame associated with the batch plot
+    """
+    run_data = run_batch(N=N, timer=timer, iterations=iterations, max_steps=max_steps)
+    run_data["Run"] += 1  # Run starts now at 1, better for plot limits
+
+    sns.set("notebook")
+    _, ax = plt.subplots(figsize=(12, 8))
+    sns.boxplot(x="timer", y="Average Velocity", hue="N", data=run_data, ax=ax)
+
+    ax.set_title(f"{run_data.shape[0]} runs of the simulation using multiple combinations of 'N' and 'timer'\nEach simulation will run for {max_steps} steps.")
+    ax.set_xlabel(f"Different timers: {timer}")
+    ax.set_ylabel("Average Velocity")
+    ax.set_xlim(-0.5, len(timer) - 0.5)
+    ax.set_ylim(0, CarAgent.max_velocity)
+
+    plt.legend()
+    plt.show()
+
+    # Returns the same data as used in the plot, run_batch() could be used if the data does not need to be the same as plot
+    return run_data
